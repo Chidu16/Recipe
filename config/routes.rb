432e3dd918f099d2b45a7d0a9afce49f3950e8f1
Priyanka <x18115144@student.ncirl.ie>
2018-11-27 15:46:00 +0000

    scaffold created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  resources :courses
+  resources :cuisines
   resources :profiles
   get '/signedinuserprofile' => 'profiles#signedinuserprofile'
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_015114) do
+ActiveRecord::Schema.define(version: 2018_11_27_142921) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "CourseType"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cuisines", force: :cascade do |t|
+    t.string "CuisineType"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "directions", force: :cascade do |t|
     t.string "Steps"
