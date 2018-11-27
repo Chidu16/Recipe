e801b73098be5dc4b61c955bfbdfd031f038745c
Chidambar Gokak <x18104291@student.ncirl.ie>
2018-11-27 14:59:25 +0000

    Adding category field to ing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_015114) do
+ActiveRecord::Schema.define(version: 2018_11_27_143927) do
 
   create_table "directions", force: :cascade do |t|
     t.string "Steps"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2018_11_21_015114) do
     t.string "Name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "profiles", force: :cascade do |t|
