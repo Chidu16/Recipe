0e3369c05c29b288f09d8b9be57a1473f9e49ea7
Priyanka <x18115144@student.ncirl.ie>
2018-11-28 16:25:43 +0000

    committing the changes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
 
-has_many :ing_linkers
-  has_many :ingredients, through: :ing_linkers
+has_many :ing_linkers, dependent: :delete_all
+  has_many :ingredients, through: :ing_linkers, dependent: :delete_all
 
   has_many :directions
   
