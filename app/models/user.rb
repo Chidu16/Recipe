6ef79d4196cd701871c2e0b3bea33b740c73720e
Priyanka <x18115144@student.ncirl.ie>
2018-12-11 12:13:26 +0000

    committing the changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,16 +19,11 @@ class User < ApplicationRecord
          has_many :notifications, dependent: :destroy
          has_many :notifylikes, dependent: :destroy
         
-<<<<<<< HEAD
-
         
-=======
-           def recipe_options
-              @recipe=Recipe.all
-              @recipe.map do |recipe|
-              [recipe.name, recipe.id]
-              end
+        def recipe_options
+            @recipe=Recipe.all
+            @recipe.map do |recipe|
+            [recipe.name, recipe.id]
+            end
         end
->>>>>>> 97cc4102a68f494c5eb2cfe26461f9f43489b447
-
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class RecipeTest < ActiveSupport::TestCase
-<<<<<<< HEAD
    # test "the truth" do
    #  assert true
    # end   
@@ -10,16 +9,6 @@ class RecipeTest < ActiveSupport::TestCase
       recipe = Recipe.new(name: "Red Pasta")
       assert_equal(false, recipe.save, "saved the recipe even though at least one of the fields were not provided!")
 
-=======
-  
-      test "the truth" do
-            assert true
-      end
-  
-   test "should not save a recipe without name" do
-     recipe = Recipe.new
-     assert_not recipe.save
->>>>>>> 97cc4102a68f494c5eb2cfe26461f9f43489b447
    end
    
 end
